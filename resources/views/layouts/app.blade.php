c7a9a532bc29f2acb4b8bc557f1289da37678399
yukawa <metalg9123@i.softbank.jp>
2023-07-02 04:21:43 +0900

    非同期処理用のjsファイル作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,5 +78,6 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="{{ asset('/js/sort.js') }}"></script>
+    <script src="{{ asset('/js/ajax.js') }}"></script>
 </body>
 </html>
